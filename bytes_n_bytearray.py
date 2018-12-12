d36ba97bf386dc7db504e95b75ae8d2411620350
matleg <ml971@hotmail.com>
2018-12-12 11:10:37 +0100

    added source for bytes + final tests

--- bytes_n_bytearray.py
+++ bytes_n_bytearray.py
@@ -1,3 +1,5 @@
+# from https://www.devdungeon.com/content/working-binary-data-python
+
 bytes_ = bytes(4)
 print('bytes_', bytes_)
 print(type(bytes_))
@@ -6,7 +8,6 @@ data = bytes(b'\xFF\xFF')  # b for binary, byte from 0 to 255 in hexadecimal, i.
 print('data', data)
 
 print('')
-
 
 # Cast bytes to bytearray
 mutable_bytes = bytearray(b'\x00\x0F')
@@ -22,11 +23,10 @@ print('immutable_bytes', immutable_bytes)
 
 print('')
 
-
 i = 16
 
 # Create one byte from the integer 16
-single_byte = i.to_bytes(1, byteorder='big', signed=True) 
+single_byte = i.to_bytes(1, byteorder='big', signed=True)
 print(single_byte)
 
 # Create four bytes from the integer
@@ -41,6 +41,10 @@ bytes_from_list = bytes([255, 254, 253, 252])
 print(bytes_from_list)
 
 # base x integer
-one_byte = [int('11110000', 2), int('0123456',7), int('12389abcdef',16)]
+one_byte = [int('11110000', 2), int('0123456', 7), int('12389abcdef', 16)]
 print(one_byte)
 
+# tests
+
+s = b'abcde1234    '  #  asii literals only
+print('binary a b space space', s[0], s[1], s[-1], s[-2])
